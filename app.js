d2bb3fd362f382316e006ef66da06d08485320bc
ankit <ankitmaurya7800@gmail.com>
2025-02-23 10:16:38 +0530

    Add console

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/database');
-const authRoutes = require('./routes/auth');
-const profileRoutes = require('./routes/profile');
-const videoRoutes = require('./routes/video');
-const app = express();
-const path = require("path");
-const fs = require('fs');
+// require('dotenv').config();
+// const express = require('express');
+// const connectDB = require('./config/database');
+// const authRoutes = require('./routes/auth');
+// const profileRoutes = require('./routes/profile');
+// const videoRoutes = require('./routes/video');
+// const app = express();
+// const path = require("path");
+// const fs = require('fs');
 
-// upload directory 
-const uploadDir = path.join(__dirname, process.env.UPLOAD_DIR || "uploads");
+// // upload directory 
+// const uploadDir = path.join(__dirname, process.env.UPLOAD_DIR || "uploads");
 
-// checking if directory not exist then create it
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+// // checking if directory not exist then create it
+// if (!fs.existsSync(uploadDir)) {
+//     fs.mkdirSync(uploadDir, { recursive: true });
+// }
 
-connectDB();
-app.use(express.json());
-app.use('/api', authRoutes);
-app.use('/api', profileRoutes);
-app.use('/api', videoRoutes);
+// connectDB();
+// app.use(express.json());
+// app.use('/api', authRoutes);
+// app.use('/api', profileRoutes);
+// app.use('/api', videoRoutes);
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+console.log("Connected")
